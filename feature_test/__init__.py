9eb16af29607f9bf82d00437944ae0a948e53672
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 21:19:02 +0000

    Reset to 37aca0398d08f882a9990207166a131366b3fd42 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -2,20 +2,48 @@
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
 
+"""
+This module contains functions for detecting the language of a given text.
+"""
 
 from fast_langdetect import detect, detect_multilingual, detect_language
 
-# 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!",low_memory=False))
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
-print(detect("hello world"))
-print(detect("你好世界"))
-print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("Hola mundo"))
-print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+def test_detect_multilingual():
+    """
+    Test the detect_multilingual function with a multilingual input.
+    """
+    print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
+
+def test_detect():
+    """
+    Test the detect function with English input.
+    """
+    print(detect("hello world"))
+
+def test_detect_language():
+    """
+    Test the detect_language function with various language inputs.
+    """
+    print(detect_language("Привет, мир!"))
+    print(detect_language("你好世界"))
+    print(detect_language("こんにちは世界"))
+    print(detect_language("안녕하세요 세계"))
+    print(detect_language("Bonjour le monde"))
+    print(detect_language("Hallo Welt"))
+    print(detect_language("Hola mundo"))
+    print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+
+def test_error_handling():
+    """
+    Test the error handling of the detect function.
+    """
+    try:
+        detect("hello world\nNEW LINE", low_memory=True)
+    except Exception as e:
+        print(f"Caught exception: {e}")
+
+# Run the tests
+test_detect_multilingual()
+test_detect()
+test_detect_language()
+test_error_handling()
