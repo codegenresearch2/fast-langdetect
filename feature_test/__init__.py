2395c92894dc20ab79c10188337f8ba9de64eee5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 22:43:09 +0000

    Reset to 37aca0398d08f882a9990207166a131366b3fd42 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -2,31 +2,19 @@
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
 
-"""
-This module provides functions to detect the language of text using the fast_langdetect library.
-It includes functions to detect the language of text in multiple languages, handle exceptions,
-and maintain consistent code style and structure.
-"""
-
 from fast_langdetect import detect, detect_multilingual, detect_language
 
 def test_language_detection():
-    """
-    测试多种语言的检测，包括英语、中文、日语、韩语、法语、德语、西班牙语等。
-    """
     # 测试多语言检测，不使用低内存模式
     result_multilingual_low_memory = detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=True)
     print(result_multilingual_low_memory)
-    # [{'lang': 'en', 'score': 0.9999999999999999}, {'lang': 'zh', 'score': 0.9999999999999999}, {'lang': 'ru', 'score': 0.9999999999999999}, {'lang': 'uk', 'score': 0.9999999999999999}, {'lang': 'ja', 'score': 0.9999999999999999}]
 
     # 测试多语言检测，使用低内存模式
     result_multilingual_no_low_memory = detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=False)
     print(result_multilingual_no_low_memory)
-    # [{'lang': 'en', 'score': 0.9999999999999999}, {'lang': 'zh', 'score': 0.9999999999999999}, {'lang': 'ru', 'score': 0.9999999999999999}, {'lang': 'uk', 'score': 0.9999999999999999}, {'lang': 'ja', 'score': 0.9999999999999999}]
 
     # 测试英语检测
-    result_english = detect("hello world")
-    print(result_english)
+    print(detect("hello world"))
 
     # 测试语言检测
     print(detect_language("Привет, мир!"))  # Output: 'ru'
