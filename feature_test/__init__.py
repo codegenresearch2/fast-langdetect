67af9c809bd79555b842a02a0a5d923d5c884664
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 21:59:27 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -4,16 +4,23 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 
-from fast_langdetect import detect, detect_multilingual, detect_langs
+from fast_langdetect import detect_language, detect_multilingual
 from fast_langdetect import parse_sentence
+
+# Comment describing the languages being tested
+print("Testing language detection with various languages:")
 
 # Directly use the functions without additional wrapper functions
 print(parse_sentence("你好世界"))
 print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
 print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
 
-print(detect("hello world"))
-print(detect_langs("Привет, мир!"))
-print(detect("こんにちは世界"))
-print(detect("안녕하세요 세계"))
-print(detect("Bonjour le monde"))
+# Add more language tests
+print(detect_language("hello world"))
+print(detect_language("こんにちは世界"))
+print(detect_language("안녕하세요 세계"))
+print(detect_language("Bonjour le monde"))
+print(detect_language("Hallo Welt"))
+print(detect_language("こんにちは世界"))
+print(detect_language("안녕하세요 세계"))
+print(detect_language("Bonjour le monde"))
