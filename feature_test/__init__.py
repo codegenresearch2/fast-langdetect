cdb414da0e60b92cb96730ccd3975245af637735
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 22:02:07 +0000

    Reset to 37aca0398d08f882a9990207166a131366b3fd42 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -9,14 +9,15 @@ This module contains the rewritten code snippet that addresses the feedback rece
 from fast_langdetect import detect, detect_multilingual, detect_language
 
 # Testing detect_multilingual function with multiple languages
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!", low_memory=True))
+# Expected output: [{'lang': 'en', 'score': 0.9999999999999999}, {'lang': 'zh', 'score': 0.9999999999999999}, {'lang': 'ru', 'score': 0.9999999999999999}]
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
 
-# Testing detect function with English input
+# Testing detect function with English and Chinese inputs
 print(detect("hello world"))
+print(detect("你好世界"))
 
 # Testing detect_language function with various languages
 print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
 print(detect_language("こんにちは世界"))
 print(detect_language("안녕하세요 세계"))
 print(detect_language("Bonjour le monde"))
