5c1c6e539cd46305c51dff604734f16bd0c428c8
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-10 22:14:50 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -4,23 +4,28 @@
 # @File    : __init__.py.py
 # @Software: PyCharm
 
-from fast_langdetect import detect_language, detect_multilingual
+from fast_langdetect import detect_language, detect_multilingual, detect
 from fast_langdetect import parse_sentence
 
 # Comment describing the languages being tested
-print("Testing language detection with various languages:")
+print("Testing language detection with various languages: English, Spanish, Traditional Chinese, Japanese, Korean, French, and German.")
 
 # Directly use the functions without additional wrapper functions
-print(parse_sentence("你好世界"))
-print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
+print(parse_sentence("你好世界"))  # Traditional Chinese
+print(parse_sentence("こんにちは世界"))  # Japanese
+print(parse_sentence("안녕하세요 세계"))  # Korean
+print(parse_sentence("Bonjour le monde"))  # French
+print(parse_sentence("Hallo Welt"))  # German
+print(parse_sentence("Hello, world!"))  # English
+print(parse_sentence("¡Hola, mundo!"))  # Spanish
+
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!¡Hola, mundo!Bonjour le monde"))
 
 # Add more language tests
-print(detect_language("hello world"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
+print(detect_language("hello world"))  # English
+print(detect_language("こんにちは世界"))  # Japanese
+print(detect_language("안녕하세요 세계"))  # Korean
+print(detect_language("Bonjour le monde"))  # French
+print(detect_language("Hallo Welt"))  # German
+print(detect_language("你好世界"))  # Traditional Chinese
+print(detect_language("¡Hola, mundo!"))  # Spanish
