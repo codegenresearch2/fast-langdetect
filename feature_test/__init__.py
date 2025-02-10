c88d5d16786c333a4225eb407fe8e4a297706ab8
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-10 21:18:49 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,21 +1,25 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
+import logging
+from fast_langdetect import detect_single_language, detect_multi_language, detect_all_languages
+from fast_langdetect import analyze_sentence
 
+# Adding logging for deprecated function
+logging.warning("The function 'parse_sentence' is deprecated. Please use 'analyze_sentence' instead.")
 
-from fast_langdetect import detect, detect_multilingual, detect_language
+print(analyze_sentence("你好世界"))
+print(analyze_sentence("你好世界！Hello, world！Привет, мир！"))
+print(detect_multi_language("Hello, world!你好世界!Привет, мир!"))
 
-# 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
+print(detect_single_language("hello world"))
+assert detect_single_language("hello world")["lang"] == "en", "Language detection error"
+assert detect_single_language("Привет, мир!")["lang"] == "ru", "Language detection error"
+print(detect_all_languages("Привет, мир!"))
 
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
-print(detect("hello world"))
-
-print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("Hola mundo"))
-print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+# Adding more language test cases for coverage
+assert detect_single_language("Hola, mundo!")["lang"] == "es", "Language detection error"
+assert detect_single_language("Bonjour le monde")["lang"] == "fr", "Language detection error"
+assert detect_single_language("Ciao mondo")["lang"] == "it", "Language detection error"
