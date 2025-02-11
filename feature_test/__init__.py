cf2fef32c545d5470fb1be69d7d085f48edce660
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 07:40:31 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -3,29 +3,17 @@
 # @Author  : sudoskys
 # @File    : __init__.py
 # @Software: PyCharm
-from fast_langdetect import detect, detect_multilingual, detect_langs
-from fast_langdetect import parse_sentence
+from fast_langdetect import detect_language, detect_multilingual
 
 # Testing different languages
-print(parse_sentence("你好世界"))  # Chinese
-print(parse_sentence("こんにちは世界"))  # Japanese
-print(parse_sentence("안녕하세요 세계"))  # Korean
-print(parse_sentence("Bonjour le monde"))  # French
-print(parse_sentence("Hallo Welt"))  # German
-print(parse_sentence("Hello, world!"))  # English
-print(parse_sentence("Привет, мир!"))  # Russian
+print(detect_language("hello world"))  # English
+print(detect_language("你好世界"))  # Chinese
+print(detect_language("こんにちは世界"))  # Japanese
+print(detect_language("안녕하세요 세계"))  # Korean
+print(detect_language("Bonjour le monde"))  # French
+print(detect_language("Hallo Welt"))  # German
+print(detect_language("Привет, мир!"))  # Russian
+print(detect_language("Hola mundo"))  # Spanish
 
 # Detecting languages in a single call
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-
-# Detecting languages individually
-print(detect("hello world"))  # English
-print(detect("你好世界"))  # Chinese
-print(detect("こんにちは世界"))  # Japanese
-print(detect("안녕하세요 세계"))  # Korean
-print(detect("Bonjour le monde"))  # French
-print(detect("Hallo Welt"))  # German
-print(detect("Привет, мир!"))  # Russian
-
-# Detecting language tags
-print(detect_langs("Привет, мир!"))
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!Hola mundo"))
