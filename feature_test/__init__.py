ec480980a4c88ddf6a973c7d63e31b06a09fd9aa
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 07:42:16 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,23 +1,35 @@
 # -*- coding: utf-8 -*-
+# @Time    : 2024/1/18 上午11:41
+# @Author  : sudoskys
+# @File    : language_detection.py
+# @Software: PyCharm
 
-from fast_langdetect import detect, detect_language, detect_multilingual
-import warnings
+from fast_langdetect import detect_language, detect_multilingual
 
 # Detect language of a single sentence
-print(detect("你好世界"))  # Expected output: {'lang': 'zh', 'prob': 0.9999999999999999}
+print(detect_language("你好世界"))  # Expected output: {'lang': 'zh', 'score': 0.9999999999999999}
 
 # Detect language of a multilingual sentence
-print(detect_language("你好世界！Hello, world！Привет, мир！"))  # Expected output: 'ZH'
+print(detect_language("你好世界！Hello, world！Привет, мир！"))  # Expected output: {'lang': 'zh', 'score': 0.9999999999999999}
 
 # Detect languages in a multilingual sentence
 print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# Expected output: [{'lang': 'en', 'prob': 0.9999999999999999}, {'lang': 'zh', 'prob': 0.9999999999999999}, {'lang': 'ru', 'prob': 0.9999999999999999}]
+# Expected output: [{'lang': 'en', 'score': 0.9999999999999999}, {'lang': 'zh', 'score': 0.9999999999999999}, {'lang': 'ru', 'score': 0.9999999999999999}]
 
 # Detect language of English sentence
-print(detect("hello world"))  # Expected output: {'lang': 'en', 'prob': 0.9999999999999999}
+print(detect_language("hello world"))  # Expected output: {'lang': 'en', 'score': 0.9999999999999999}
 
 # Detect language of Russian sentence
-print(detect("Привет, мир!"))  # Expected output: {'lang': 'ru', 'prob': 0.9999999999999999}
+print(detect_language("Привет, мир!"))  # Expected output: {'lang': 'ru', 'score': 0.9999999999999999}
 
-# Deprecation warning for using 'detect_langs'
-warnings.warn("The 'detect_langs' function is deprecated. Use 'detect_multilingual' instead.", DeprecationWarning)
+# Detect language of Japanese sentence
+print(detect_language("こんにちは世界"))  # Expected output: {'lang': 'ja', 'score': 0.9999999999999999}
+
+# Detect language of Korean sentence
+print(detect_language("안녕하세요 세계"))  # Expected output: {'lang': 'ko', 'score': 0.9999999999999999}
+
+# Detect language of French sentence
+print(detect_language("Bonjour le monde"))  # Expected output: {'lang': 'fr', 'score': 0.9999999999999999}
+
+# Detect language of German sentence
+print(detect_language("Hallo Welt"))  # Expected output: {'lang': 'de', 'score': 0.9999999999999999}
