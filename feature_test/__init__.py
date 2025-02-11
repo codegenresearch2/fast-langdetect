7428217fd8435583ca6bd2581e3f6948839444de
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 08:21:07 +0000

    Reset to 37aca0398d08f882a9990207166a131366b3fd42 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -2,18 +2,24 @@
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
 
+"""
+This script tests the language detection functionalities provided by the fast_langdetect library.
+"""
+
 from fast_langdetect import detect, detect_multilingual, detect_language
 
 # Test cases for detect_multilingual function
-print(detect_multilingual("Hello, world! 你好世界! Привет, мир!"))
-print(detect_multilingual("こんにちは世界"))
-print(detect_multilingual("안녕하세요 세계"))
-print(detect_multilingual("Bonjour le monde"))
-print(detect_multilingual("Hallo Welt"))
-print(detect_multilingual("Hola mundo"))
-print(detect_multilingual("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+print("Testing detect_multilingual with mixed languages:")
+print(detect_multilingual("Hello, world! 你好世界! Привет, мир!", low_memory=True))
+print(detect_multilingual("こんにちは世界", low_memory=False))
+print(detect_multilingual("안녕하세요 세계", low_memory=True))
+print(detect_multilingual("Bonjour le monde", low_memory=False))
+print(detect_multilingual("Hallo Welt", low_memory=True))
+print(detect_multilingual("Hola mundo", low_memory=False))
+print(detect_multilingual("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等", low_memory=True))
 
 # Test cases for detect function
+print("\nTesting detect function with various languages:")
 print(detect("hello world"))
 print(detect("你好世界"))
 print(detect("こんにちは世界"))
@@ -24,6 +30,7 @@ print(detect("Hola mundo"))
 print(detect("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
 
 # Test cases for detect_language function
+print("\nTesting detect_language function with different languages:")
 print(detect_language("hello world"))
 print(detect_language("你好世界"))
 print(detect_language("こんにちは世界"))
