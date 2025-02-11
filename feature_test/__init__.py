2f5abda5211d71539c93998ed80419aee2a998d0
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 09:55:35 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,19 +1,26 @@
-import warnings
+# -*- coding: utf-8 -*-
+# @Time    : 2024/1/18 上午11:41
+# @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
 
-from fast_langdetect import detect_language, detect_multilingual
+from fast_langdetect import detect, detect_language, detect_multilingual
 
-def parse_text(text):
-    return detect_language(text)
+# Detect language of a single sentence
+print(detect("你好世界"))
 
-def detect_languages(text):
-    return detect_multilingual(text)
+# Detect language of a multilingual sentence
+print(detect_language("你好世界！Hello, world！Привет, мир！"))
 
-print(parse_text("你好世界"))
-print(parse_text("你好世界！Hello, world！Привет, мир！"))
-print(detect_languages("Hello, world!你好世界!Привет, мир!"))
+# Detect languages in a multilingual sentence
+print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
 
-warnings.warn("The 'detect' function is deprecated. Use 'parse_text' instead.", DeprecationWarning)
-print(parse_text("hello world"))
+# Demonstrate language detection for various languages
+print(detect("hello world"))
+print(detect("Bonjour le monde"))
+print(detect("Hallo Welt"))
+print(detect("こんにちは世界"))
+print(detect("안녕하세요 세계"))
 
-warnings.warn("The 'detect_langs' function is deprecated. Use 'detect_languages' instead.", DeprecationWarning)
-print(detect_languages("Привет, мир!"))
+# Detect languages in a sentence with multiple languages
+print(detect_multilingual("Привет, мир!"))
