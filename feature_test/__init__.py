a591323e1baa851687d8f397cc2ea4aed0b9b0e5
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-eu-1>
2025-02-11 09:39:37 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2024/1/18 上午11:41
-# @Author  : sudoskys
+import warnings
 
+from fast_langdetect import detect_language, detect_multilingual
 
-from fast_langdetect import detect, detect_multilingual, detect_language
+def parse_text(text):
+    return detect_language(text)
 
-# 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
+def detect_languages(text):
+    return detect_multilingual(text)
 
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
-print(detect("hello world"))
+print(parse_text("你好世界"))
+print(parse_text("你好世界！Hello, world！Привет, мир！"))
+print(detect_languages("Hello, world!你好世界!Привет, мир!"))
 
-print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("Hola mundo"))
-print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+warnings.warn("The 'detect' function is deprecated. Use 'parse_text' instead.", DeprecationWarning)
+print(parse_text("hello world"))
+
+warnings.warn("The 'detect_langs' function is deprecated. Use 'detect_languages' instead.", DeprecationWarning)
+print(detect_languages("Привет, мир!"))
