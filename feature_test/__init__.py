6777effb1ad5cf13e9cda8c7c2cdb1fe4b467c31
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 07:07:06 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2024/1/18 上午11:41
 # @Author  : sudoskys
+# @File    : __init__.py.py
+# @Software: PyCharm
 
+import warnings
+from fast_langdetect import detect, detect_multilingual, detect_langs
+from fast_langdetect import parse_sentence
 
-from fast_langdetect import detect, detect_multilingual, detect_language
+def consolidate_language_detection(text):
+    result = detect_multilingual(text, low_memory=True)
+    return result[0].get("lang") if result else None
 
-# 测试繁体，简体，日文，英文，韩文，法文，德文，西班牙文
+def deprecated_function_warning(old_func, new_func):
+    warnings.warn(f"The function {old_func} is deprecated. Please use {new_func} instead.", DeprecationWarning)
 
-print(detect_multilingual("Hello, world!你好世界!Привет, мир!"))
-# [{'lang': 'ja', 'score': 0.32009604573249817}, {'lang': 'uk', 'score': 0.27781224250793457}, {'lang': 'zh', 'score': 0.17542070150375366}, {'lang': 'sr', 'score': 0.08751443773508072}, {'lang': 'bg', 'score': 0.05222449079155922}]
+# Deprecate old functions
+deprecated_function_warning("detect", "consolidate_language_detection")
+deprecated_function_warning("detect_langs", "consolidate_language_detection")
+
+print(parse_sentence("你好世界"))
+print(parse_sentence("你好世界！Hello, world！Привет, мир！"))
+print(consolidate_language_detection("Hello, world!你好世界!Привет, мир!"))
+
 print(detect("hello world"))
-
-print(detect_language("Привет, мир!"))
-print(detect_language("你好世界"))
-print(detect_language("こんにちは世界"))
-print(detect_language("안녕하세요 세계"))
-print(detect_language("Bonjour le monde"))
-print(detect_language("Hallo Welt"))
-print(detect_language("Hola mundo"))
-print(detect_language("這些機構主辦的課程，多以基本電腦使用為主，例如文書處理、中文輸入、互聯網應用等"))
+print(detect_langs("Привет, мир!"))
