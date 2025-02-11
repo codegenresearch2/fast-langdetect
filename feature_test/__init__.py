4eeb1a01a44260ac7c60ed0d6deb5d07d93c8f3d
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 08:43:18 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- feature_test/__init__.py
+++ feature_test/__init__.py
@@ -8,17 +8,25 @@ from fast_langdetect import detect, detect_language, detect_multilingual
 # Import statements should be in the same order as in the gold code
 # from fast_langdetect import detect, detect_language, detect_multilingual
 
-# Testing different languages
-print(detect_language("hello world"))  # English
-print(detect_language("你好世界"))  # Chinese
-print(detect_language("こんにちは世界"))  # Japanese
-print(detect_language("안녕하세요 세계"))  # Korean
-print(detect_language("Bonjour le monde"))  # French
-print(detect_language("Hallo Welt"))  # German
-print(detect_language("Привет, мир!"))  # Russian
-print(detect_language("Hola mundo"))  # Spanish
+# 测试不同语言的检测
+print(detect_language("hello world"))  # 英语
+print(detect_language("你好世界"))  # 中文
+print(detect_language("こんにちは世界"))  # 日语
+print(detect_language("안녕하세요 세계"))  # 韩语
+print(detect_language("Bonjour le monde"))  # 法语
+print(detect_language("Hallo Welt"))  # 德语
+print(detect_language("Привет, мир!"))  # 俄语
+print(detect_language("Hola mundo"))  # 西班牙语
 
-# Detecting languages in a single call
-print(detect("hello world"))  # English
-print(detect("你好世界"))  # Chinese
+# 检测多种语言
 print(detect_multilingual("Hello, world!你好世界!Привет, мир!Hola mundo"))
+
+# 检测特定语言
+print(detect("hello world"))  # 英语
+print(detect("你好世界"))  # 中文
+print(detect("こんにちは世界"))  # 日语
+print(detect("안녕하세요 세계"))  # 韩语
+print(detect("Bonjour le monde"))  # 法语
+print(detect("Hallo Welt"))  # 德语
+print(detect("Привет, мир!"))  # 俄语
+print(detect("Hola mundo"))  # 西班牙语
