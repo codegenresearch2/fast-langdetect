a828a8bd0aa04855f5c7a3210e2cd442cb5752ac
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 13:36:27 +0000

    Reset to 17b159ae7ee2eaa33dcf4014810644a12a0cb6b4 and made changes for LLM Round 1

--- src/fast_langdetect/__init__.py
+++ src/fast_langdetect/__init__.py
@@ -1,3 +1,13 @@
-# -*- coding: utf-8 -*-
+import warnings
 
-from .ft_detect import detect, detect_language, detect_langs, detect_multilingual  # noqa: F401
+from .ft_detect import detect as _detect, detect_langs, detect_multilingual as _detect_multilingual
+
+def detect_language(text):
+    warnings.warn("Function 'detect_language' is deprecated. Please use 'detect' instead.", DeprecationWarning)
+    return _detect(text)['lang'].upper()
+
+def detect_text(text):
+    return _detect(text)
+
+def detect_languages(text):
+    return _detect_multilingual(text)
